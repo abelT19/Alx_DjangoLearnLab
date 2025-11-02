134bc420444387e9220c22878174e6149e1e2a73
abelT19 <abeltariku43@gmail.com>
2025-11-02 05:17:16 -0800

    Fix syntax errors in admin.py

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 "from .models import Book"]
 "register", "admin.ModelAdmin"]
+ ["list_filter", "author", "publication_year"]
+
 
 # Register your models here.
+
 
 
