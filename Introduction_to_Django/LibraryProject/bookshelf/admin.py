5c13fd1d28036e12fd8cb75fefe7190f1b321d3f
abelT19 <abeltariku43@gmail.com>
2025-11-02 05:17:51 -0800

    Update admin.py

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 "from .models import Book"]
 "register", "admin.ModelAdmin"]
  ["list_filter", "author", "publication_year"]
-
+["search_fields", "title"]
 
 # Register your models here.
+
 
 
 
