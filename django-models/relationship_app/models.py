b5d02b7ca5d29d596aeeec09fd02d97c08069393
abelT19 <abeltariku43@gmail.com>
2025-11-09 12:07:26 -0800

    Add UserProfile model and post_save signal
    Added UserProfile model with role choices and signal to create profile on user creation.

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,3 +1,21 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from django.db import models
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = [('Admin','Admin'),('Librarian','Librarian'),('Member','Member')]
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance, role='Member')
+
+
+
+
 from django.db import models
 
 # Author model
@@ -30,3 +48,4 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+
