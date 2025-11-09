e47f71f6bf2c4dd001998410c82beabeb25470cd
abelT19 <abeltariku43@gmail.com>
2025-11-09 12:18:16 -0800

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,48 +1,38 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+from .forms import BookForm  # Assume you have a ModelForm for Book
 
-def is_admin(user):
-    return user.is_authenticated and hasattr(user, 'profile') and user.profile.role == 'Admin'
-
-@user_passes_test(is_admin, login_url='/login/')
-def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
-
-
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login, logout
-from django.shortcuts import redirect
-
-# Registration view
-def register_view(request):
+# Add a book
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = BookForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
+            form.save()
             return redirect('list_books')
     else:
-        form = UserCreationForm()
-    return render(request, 'relationship_app/register.html', {'form': form})
+        form = BookForm()
+    return render(request, 'relationship_app/book_form.html', {'form': form})
 
-# Login view
-def login_view(request):
+# Edit a book
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
+        form = BookForm(request.POST, instance=book)
         if form.is_valid():
-            user = form.get_user()
-            login(request, user)
+            form.save()
             return redirect('list_books')
     else:
-        form = AuthenticationForm()
-    return render(request, 'relationship_app/login.html', {'form': form})
+        form = BookForm(instance=book)
+    return render(request, 'relationship_app/book_form.html', {'form': form})
 
-# Logout view
-def logout_view(request):
+# Delete a book
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
-        logout(request)
-        return redirect('login')
-    return render(request, 'relationship_app/logout.html')
-
-
+        book.delete()
+        return redirect('list_books')
+    return render(request, 'relationship_app/book_confirm_delete.html', {'book': book})
