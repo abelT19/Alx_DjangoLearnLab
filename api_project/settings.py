4d3e5ce12bc43413e14200e94117b0822bfb41c2
abelT19 <abeltariku43@gmail.com>
2025-11-23 08:33:18 -0800

    Update settings.py

--- api_project/settings.py
+++ api_project/settings.py
@@ -1,4 +1,16 @@
 INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',           # already included
+    'rest_framework.authtoken', # ✅ ADD THIS LINE
+    'api',                      # your app
+]
+
+INSTALLED_APPS = [
     ...
     'rest_framework',
     'rest_framework.authtoken',
@@ -13,4 +25,5 @@ REST_FRAMEWORK = {
     ],
 }
 python manage.py migrate
+
 
