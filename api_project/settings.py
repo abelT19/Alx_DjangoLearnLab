9ec7f9335a6d52730cf28cb7d77406e4caf6c405
abelT19 <abeltariku43@gmail.com>
2025-11-23 08:28:37 -0800

    Reorder INSTALLED_APPS to include auth token

--- api_project/settings.py
+++ api_project/settings.py
@@ -1,16 +1,6 @@
-"rest_framework.authtoken"
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
-    # Django REST Framework
+    ...
     'rest_framework',
-
-    # Custom app
+    'rest_framework.authtoken',
     'api',
 ]
-
