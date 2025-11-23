c04e795c1ccbd0147a2fedc48dd68ef679d5fe5e
abelT19 <abeltariku43@gmail.com>
2025-11-23 08:40:04 -0800

    Update settings.py

--- api_project/settings.py
+++ api_project/settings.py
@@ -1,11 +1,9 @@
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework.authtoken',  # <-- ADD THIS LINE
-    'api',
-]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
