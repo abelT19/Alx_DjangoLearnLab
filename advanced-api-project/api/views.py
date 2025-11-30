b36bb460947c3fda0a3c30d03afd6f6f2dc457d0
abelT19 <abeltariku43@gmail.com>
2025-11-30 11:38:02 -0800

    Add files via upload

---
+++ advanced-api-project/api/views.py
@@ -0,0 +1,28 @@
+from rest_framework import viewsets
+from rest_framework import permissions
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
+
+# -----------------------------
+# Book ViewSet
+# -----------------------------
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Books to be viewed or edited.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]  # Change to IsAuthenticated if needed
+
+
+# -----------------------------
+# Author ViewSet
+# -----------------------------
+class AuthorViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Authors to be viewed or edited.
+    Includes nested Book data automatically.
+    """
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [permissions.AllowAny]  # Change to IsAuthenticated if needed
