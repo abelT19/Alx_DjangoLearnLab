a7b1e99373a03fc7dabef1408a5ef47468819e00
abelT19 <abeltariku43@gmail.com>
2025-12-14 09:44:18 -0800

    Add files via upload

---
+++ social_media_api/social_media_api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import RegisterView, CustomAuthToken, UserProfileView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', CustomAuthToken.as_view(), name='login'),
+    path('profile/<int:pk>/', UserProfileView.as_view(), name='profile'),
+]
