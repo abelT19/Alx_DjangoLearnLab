68c0d19f4f99ddd83c4c4dbe3ca2ce4704eefa78
abelT19 <abeltariku43@gmail.com>
2025-12-14 09:34:44 -0800

    Add files via upload

---
+++ social_media_api/social_media_api/settings.py
@@ -0,0 +1,22 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    
+    # Third-party apps
+    'rest_framework',
+    'rest_framework.authtoken',  # For token authentication
+
+    # Local apps
+    'accounts',
+]
+
+# Django REST Framework configuration
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
